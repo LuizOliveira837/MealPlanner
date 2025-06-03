67746da91fe177d156646bf09d76f260d4dfdff5
LuizOliveira837 <luizhorocha@gmail.com>
2025-06-03 20:19:03 -0300

    feat: criando conexão com sql server + migrations

---
+++ src/backend/MealPlanner.Persistence/Interfaces/PatientRepository.cs
@@ -0,0 +1,43 @@
+﻿using MealPlanner.Domain;
+using MealPlanner.Domain.Interfaces;
+using MealPlanner.Persistence.Database;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MealPlanner.Persistence.Interfaces
+{
+    public class PatientRepository : IPatientRepository
+    {
+        private readonly MealPlannerContext _context;
+
+        public PatientRepository(MealPlannerContext context)
+        {
+            _context = context;
+        }
+        public async Task<Guid> Create(Patient patient)
+        {
+            await _context.AddAsync(patient);
+            await _context.SaveChangesAsync();
+
+            return patient.Id;
+        }
+
+        public Task<Guid> Delete(Guid id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Patient> GetById(Guid id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task Update()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
