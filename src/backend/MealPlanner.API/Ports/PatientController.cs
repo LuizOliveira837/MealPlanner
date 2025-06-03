1fba56949a0191af54cec01625771f8b4190c2e6
LuizOliveira837 <luizhorocha@gmail.com>
2025-06-02 23:34:06 -0300

    feat: Add estrutura inicial

---
+++ src/backend/MealPlanner.API/Ports/PatientController.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MealPlanner.API.Ports
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class PatientController : Controller
+    {
+        [HttpPost]
+        public async Task<IActionResult> Post()
+        {
+            return Ok();
+        }
+
+        [HttpGet("{id:int}")]
+        public async Task<IActionResult> GetById(int id)
+        {
+            return Ok();
+        }
+
+        [HttpDelete("{id:int}")]
+        public async Task<IActionResult> DeleteById(int id)
+        {
+            return Ok();
+        }
+
+        [HttpPut("{id:int}")]
+        public async Task<IActionResult> Put(int id)
+        {
+            return Ok();
+        }
+    }
+}
